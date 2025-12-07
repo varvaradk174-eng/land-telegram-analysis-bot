c22792d44b9c19b00a8cc5b67efda949746449d0
varvaradk174-eng <varvaradk174@gmail.com>
2025-12-07 16:49:29 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import threading
+from bot import run_bot
 
 app = Flask(__name__)
 
 @app.get("/")
 def home():
     return "Bot is running!"
+
+# Запускаем бота в отдельном потоке
+def start_bot():
+    run_bot()
+
+bot_thread = threading.Thread(target=start_bot)
+bot_thread.daemon = True
+bot_thread.start()
