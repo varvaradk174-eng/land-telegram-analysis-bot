04aed8d22ffc3b9e9a674cc8e8c910478b24ad78
varvaradk174-eng <varvaradk174@gmail.com>
2025-12-07 17:40:03 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import os
+import threading
+from bot import run_bot
 
 app = Flask(__name__)
 
@@ -7,6 +9,13 @@ app = Flask(__name__)
 def home():
     return "Server is running!"
 
+def start_bot():
+    run_bot()
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    # Запускаем бота в отдельном потоке
+    bot_thread = threading.Thread(target=start_bot)
+    bot_thread.start()
+
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
